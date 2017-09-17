b7adca454256acc73639f91d0f598071ad3f89df
zxzx1290 <indexzexx@gmail.com>
2017-09-17 20:15:41 +0800

    fix ssr option

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,4 +37,4 @@ forwarded_for transparent' > /etc/squid3/squid.conf
 
 service squid start
 
-/usr/local/bin/ssserver "-s 0.0.0.0 -p 6080 -k china -m aes-256-cfb -o http_simple -O auth_sha1_v4"
+/usr/local/bin/ssserver -s 0.0.0.0 -p 6080 -k china -m aes-256-cfb -o http_simple -O auth_sha1_v4
